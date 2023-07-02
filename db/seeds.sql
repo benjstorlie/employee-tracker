f251572b58d9033f8b9a645c454452319545c448
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-01 23:50:18 -0700

    fixed some recursion problems

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,12 +9,12 @@ INSERT INTO departments (department_name) VALUES
 
 -- Inserting roles
 INSERT INTO roles (role_name, salary, is_manager, department_id) VALUES
-  ('Math Teacher', 50000, 0, 1),
-  ('Science Teacher', 55000, 0, 2),
-  ('English Teacher', 48000, 0, 3),
+  ('Math Teacher', 50000, 1, 1),
+  ('Science Teacher', 55000, 1, 2),
+  ('English Teacher', 48000, 1, 3),
   ('Principal', 80000, 1, 5),
   ('Vice Principal', 70000, 1, 5),
-  ('History Teacher', 52000, 0, 4),
+  ('History Teacher', 52000, 1, 4),
   ('Paraprofessional', 35000, 0, 6),
   ('1:1 Paraprofessional', 38000, 0, 6),
   ('Secretary', 40000, 0, 5),
@@ -25,36 +25,29 @@ INSERT INTO roles (role_name, salary, is_manager, department_id) VALUES
 
 -- Inserting employees
 INSERT INTO employees (first_name, last_name, role_id, manager_id, entry_date) VALUES
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id, entry_date) VALUES
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id, entry_date) VALUES
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id, entry_date) VALUES
-
-  1('John', 'Smith', 4, NULL, '2022-01-15'),
-  2('Alice', 'Johnson', 5, 4, '2022-02-01'),
-  3('Bob', 'Williams', 1, 4, '2022-03-10'),
-  4('Charlie', 'Davis', 2, 5, '2022-04-03'),
-  5('Eva', 'Brown', 3, 4, '2022-05-22'),
-  6('Frank', 'Miller', 6, 5, '2022-06-08'),
-  7('Grace', 'Anderson', 2, 4, '2022-07-17'),
-  8('Henry', 'Taylor', 1, 5, '2022-08-04'),
-  9('Isabella', 'Moore', 6, 4, '2022-09-21'),
-  10('Jack', 'Clark', 3, 5, '2022-10-11'),
-  11('Kate', 'Lee', 2, 4, '2022-11-28'),
-  12('Liam', 'Wilson', 1, 5, '2022-12-09'),
-  13('Sarah', 'Jones', 7, 1, '2022-01-03'),
-  ('Michael', 'Thompson', 7, 1, '2022-01-03'),
-  ('Emma', 'Davis', 8, 2, '2022-01-03'),
-  ('Olivia', 'Johnson', 8, 3, '2022-01-03'),
-  ('Sophia', 'Smith', 9, 5, '2022-01-03'),
-  ('Daniel', 'Brown', 9, 5, '2022-01-03'),
-  ('Emily', 'Taylor', 10, 5, '2022-01-03'),
-  ('William', 'Miller', 11, 5, '2022-01-03'),
-  ('Alexander', 'Anderson', 11, 5, '2022-01-03'),
-  ('Ava', 'White', 12, 5, '2022-01-03'),
-  ('James', 'Robinson', 12, 5, '2022-01-03'),
-  ('Abigail', 'Harris', 12, 5, '2022-01-03'),
-  ('Natalie', 'Lewis', 12, 5, '2022-01-03'),
-  ('Ryan', 'Moore', 12, 5, '2022-01-03');
+  ("Oliver", "Johnson", 4, NULL, '2022-01-15'),
+  ("Emma", "Larson", 5, NULL, '2022-02-01'),
+  ("Mateo", "Gonzalez", 8, 2, '2022-01-03'),
+  ("Safiya", "Mohamed", 8, 2, '2022-01-03'),
+  ("Kou", "Vang", 1, 1, '2022-03-10'),
+  ("Evelyn", "Andersen", 2, 1, '2022-04-03'),
+  ("Carmen", "Lopez", 3, 1, '2022-05-22'),
+  ("Sofia", "Hernandez", 6, 1, '2022-06-08'),
+  ('Grace', 'Anderson', 2, 1, '2022-07-17'),
+  ("Isaac", "Nelson", 1, 1, '2022-08-04'),
+  ("Liam", "Johansson", 6, 1, '2022-09-21'),
+  ("Mustafa", "Hassan", 3, 1, '2022-10-11'),
+  ("Kate", "Lee", 2, 1, '2022-11-28'),
+  ("Anya", "Lund", 1, 1, '2022-12-09'),
+  ("Sophia", "Smith", 9, 1, '2022-01-03'),
+  ("Daniel", "Brown", 9, 1, '2022-01-03'),
+  ('Emily', 'Taylor', 10, 1, '2022-01-03'),
+  ('William', 'Miller', 11, 1, '2022-01-03'),
+  ("Alexander", "Anderson", 12, 1, '2022-01-03'),
+  ('Mohammed', 'Mohammed', 13, 1, '2022-01-03'),
+  ("Elena", "Garcia", 7, 5, '2022-01-03'),
+  ("Mohamed", "Ali", 7, 6, '2022-01-03'),
+  ("Shawnte", "White", 7, 11, '2022-01-03'),
+  ("James", "Robinson", 7, 12, '2022-01-03'),
+  ("Abigail", "Harris", 7, 9, '2022-01-03'),
+  ("Sara", "Xiong", 7, 13, '2022-01-03');
