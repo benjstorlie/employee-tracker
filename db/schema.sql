81c15d01e16cff78bea3f9a6c5f50d161d61ed00
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-01 23:50:04 -0700

    added many useful 'views'

--- db/schema.sql
+++ db/schema.sql
@@ -33,3 +33,49 @@ CREATE TABLE employees (
   REFERENCES employees(id)
   ON DELETE SET NULL
 );
+
+CREATE VIEW full_employees_table AS
+  SELECT
+    e.first_name AS "First Name",
+    e.last_name AS "Last Name",
+    r.role_name AS "Role",
+    CONCAT(m.first_name, ' ', m.last_name) AS Manager,
+    mr.role_name AS "Manager Role"
+  FROM
+    employees AS e
+    LEFT JOIN roles AS r ON e.role_id = r.id
+    LEFT JOIN employees AS m ON e.manager_id = m.id
+    LEFT JOIN roles AS mr ON m.role_id = mr.id
+  ORDER BY r.id, mr.id;
+
+CREATE VIEW full_roles_table AS
+  SELECT
+    r.role_name AS "Role",
+    d.department_name AS "Department",
+    COUNT(e.role_id) AS "Number of Employees"
+  FROM
+    roles AS r
+    LEFT JOIN departments AS d ON r.department_id = d.id
+    LEFT JOIN employees AS e ON r.id = e.role_id
+  GROUP BY r.id;
+
+CREATE VIEW full_departments_table AS
+  SELECT
+    d.department_name AS "Department",
+    COUNT(e.role_id) AS "Number of Employees"
+  FROM
+    departments AS d
+    LEFT JOIN roles AS r ON r.department_id = d.id
+    LEFT JOIN employees AS e ON r.id = e.role_id
+  GROUP BY d.department_name;
+
+CREATE VIEW managers AS
+  SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS Name,
+    r.role_name AS Role
+  FROM
+    employees AS e
+    LEFT JOIN roles AS r ON r.id = e.role_id
+  WHERE
+    r.is_manager = true;
+    
